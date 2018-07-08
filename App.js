bc125cfdd2a463a9d12602f5e019c9357d3b7c4b
方正 <215566435@qq.com>
2018-07-08 21:49:34 +0800

    chore: add fetch

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, ScrollView, Alert} from 'react-native';
 import {Provider} from 'react-redux';
 import {Button} from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 import Rluy from './src/utils/rluy.native';
 import user from './src/controller/user';
@@ -12,51 +13,76 @@ import KeyboardDetector from './src/utils/keyboard';
 Rluy.addController(user);
 const store = Rluy.run();
 
+let didMove = false;
 export default class App extends React.Component {
+  getViewContainerRef = node => (this.View = node);
+
+  componentDidMount() {
+    setInterval(() => {
+      fetch('http://18.222.175.208/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          a: 'ur',
+          e: '215566435@qq.com',
+          p: 'metal_gear2',
+        }),
+      }).then(res => {
+        console.log(res);
+        res.json().then(json => {
+          console.log(json);
+        });
+      });
+    }, 5000);
+  }
+
   render() {
     return (
       <Provider store={store}>
         <KeyboardDetector>
-          {(isShow, height) => (
+          {(isShow, height, getViewContainerRef) => (
             <ScrollView
               contentContainerStyle={{
                 backgroundColor: '#2D59D9',
                 height: '100%',
-                paddingBottom: isShow ? height : 0,
               }}>
-              <View style={{height: '50%'}} />
-              <View style={{height: '50%', backgroundColor: 'white'}}>
-                <Form
-                  onSumit={obj => {
-                    Alert.alert('title', JSON.stringify(obj));
-                  }}>
-                  {(onChange, onSumit) => (
-                    <React.Fragment>
-                      <Input
-                        placeholder="Name"
-                        onChangeText={t => onChange({key: 'Name', value: t})}
-                      />
-                      <Input
-                        placeholder="Email"
-                        onChangeText={t => onChange({key: 'Email', value: t})}
-                      />
-                      <Input
-                        placeholder="Password"
-                        onChangeText={t =>
-                          onChange({key: 'Password', value: t})
-                        }
-                      />
-                      <Button
-                        buttonStyle={{height: 48, marginTop: 16}}
-                        backgroundColor="#0077FF"
-                        borderRadius={4}
-                        onPress={onSumit}
-                        title="Sign up"
-                      />
-                    </React.Fragment>
-                  )}
-                </Form>
-              </View>
+              <Animatable.View ref={getViewContainerRef}>
+                <View style={{height: '50%'}} />
+                <View style={{height: '50%', backgroundColor: 'white'}}>
+                  <Form
+                    onSumit={obj => {
+                      Alert.alert('title', JSON.stringify(obj));
+                    }}>
+                    {(onChange, onSumit) => (
+                      <React.Fragment>
+                        <Input
+                          placeholder="Name"
+                          onChangeText={t => onChange({key: 'Name', value: t})}
+                        />
+                        <Input
+                          placeholder="Email"
+                          onChangeText={t => onChange({key: 'Email', value: t})}
+                        />
+                        <Input
+                          placeholder="Password"
+                          onChangeText={t =>
+                            onChange({key: 'Password', value: t})
+                          }
+                        />
+                        <Button
+                          buttonStyle={{height: 48, marginTop: 16}}
+                          backgroundColor="#0077FF"
+                          borderRadius={4}
+                          onPress={onSumit}
+                          title="Sign up"
+                        />
+                      </React.Fragment>
+                    )}
+                  </Form>
+                </View>
+              </Animatable.View>
               )}
             </ScrollView>
           )}
--- src/utils/keyboard.js
+++ src/utils/keyboard.js
@@ -5,7 +5,10 @@ export default class KeyboardDetector extends React.Component {
   state = {
     isShow: false,
     height: 0,
+    didMove: false,
   };
+
+  getViewContainerRef = node => (this.View = node);
 
   componentDidMount() {
     this.keyboardDidShowListener = Keyboard.addListener(
@@ -24,19 +27,36 @@ export default class KeyboardDetector extends React.Component {
   }
 
   _keyboardDidShow = e => {
+    if (!this.state.didMove) {
+      this.View.transition(
+        {paddingBottom: 0},
+        {paddingBottom: e.endCoordinates.height}
+      );
+    }
+
     this.setState({
       height: e.endCoordinates.height,
       isShow: true,
+      didMove: true,
     });
   };
 
   _keyboardDidHide = () => {
+    this.View.transition(
+      {paddingBottom: this.state.height},
+      {paddingBottom: 0}
+    );
     this.setState({
       isShow: false,
+      didMove: false,
     });
   };
 
   render() {
-    return this.props.children(this.state.isShow, this.state.height);
+    return this.props.children(
+      this.state.isShow,
+      this.state.height,
+      this.getViewContainerRef
+    );
   }
 }
