692dddb4a2bc6e254e2a12339028a7422b880b8b
方正 <215566435@qq.com>
2018-07-27 00:21:58 +0800

    chore: android margin

--- src/pages/PostJob/index.js
+++ src/pages/PostJob/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, ScrollView} from 'react-native';
+import {View, ScrollView, Platform} from 'react-native';
 import {Page} from '../../components/PageHOC';
 import {WIDTH} from '../../utils/plaform';
 import List from '../../components/List';
@@ -17,29 +17,32 @@ export default class PostJob extends React.Component {
           justifyContent: 'center',
           alignItems: 'center',
         }}>
-        <ScrollView style={{height: '100%', width: WIDTH, marginTop: 20}}>
+        <ScrollView
+          style={{
+            height: '100%',
+            width: WIDTH,
+            marginTop: Platform.OS === 'ios' ? 20 : 0,
+          }}>
           <List>
-            <Item key="1" title="Company" desc="WestWorld" />
+            <Item key="1" title="Company" desc="Edit" />
           </List>
           <List>
             <Item key="1" title="Job Type" desc="Choose" />
-            <Item key="2" title="Job Title" desc="Choose" />
+            <Item key="2" title="Job Title" desc="Edit" />
             <Item key="3" title="Categories" desc="Choose" />
           </List>
           <List>
             <Item key="1" title="Region" desc="Choose" />
             <Item key="2" title="District" desc="Choose" />
-            <Item key="3" title="Location" desc="Choose" />
+            <Item key="3" title="Location" desc="Edit" />
           </List>
           <List>
             <Item key="1" title="Pay Type" desc="Choose" />
             <Item key="2" title="Pay Range" desc="Choose" />
           </List>
-          <Button
-            title="Post a job"
-            backgroundColor="#2D59D9"
-            style={{marginTop: 8, marginBottom: 8}}
-          />
+          <View style={{marginTop: 8, marginBottom: 8}}>
+            <Button title="Post a job" backgroundColor="#2D59D9" />
+          </View>
         </ScrollView>
       </View>
     );
