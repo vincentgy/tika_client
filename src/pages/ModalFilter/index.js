bab8b6b99c3f9f272c4e9777ef72ceb06df07bdc
方正 <215566435@qq.com>
2018-08-28 22:19:03 +0800

    add sagmentedcontroll

--- src/pages/ModalFilter/index.js
+++ src/pages/ModalFilter/index.js
@@ -17,219 +17,34 @@ import JobType from './type';
 import SelectItem from '../../public/SelectItem';
 import {connect} from 'react-redux';
 import {produce} from 'immer';
+import SegmentedControlTab from 'react-native-segmented-control-tab'; //https://github.com/kirankalyan5/react-native-segmented-control-tab#props
 
-const FilterContainer = styled.View`
-  height: 40px;
-  border-bottom-width: 1px;
-  border-bottom-color: rgba(120, 120, 120, 0.2);
-  background-color: white;
-  flex-direction: row;
-  align-items: center;
-`;
-
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const {defaultState} = props;
-
+class Filter extends React.Component {
+  constructor() {
+    super();
     this.state = {
-      currentState: defaultState,
+      selectedIndex: 0,
     };
   }
 
-  select = state => {
+  handleIndexChange = index => {
     this.setState({
-      currentState: state,
+      ...this.state,
+      selectedIndex: index,
     });
   };
-
-  confirm = () => {
-    this.props.confirm && this.props.confirm(this.state.currentState);
-  };
-
   render() {
     return (
-      <View>
-        {this.props.children(this.select, this.state.currentState)}
-        <ButtonGroup comfirm={this.confirm} />
-      </View>
-    );
-  }
-}
-
-class Categoris extends React.Component {
-  state = {
-    _categories: {},
-  };
-
-  componentDidMount() {
-    const {categories} = this.props;
-    this.setState({
-      _categories: categories,
-    });
-  }
-
-  onChange = ({id, name}) => {
-    const newState = produce(this.state._categories, draft => {
-      if (draft[name] === void 666) {
-        draft[name] = {id, name};
-      } else {
-        draft[name] = void 666;
-      }
-    });
-    this.setState(
-      {
-        _categories: newState,
-      },
-      () => this.props.onChange && this.props.onChange(this.state._categories)
-    );
-  };
-
-  render() {
-    return (
-      <ScrollView style={{height: 350}}>
-        <FetchCategoris
-          cache
-          selected={this.state._categories}
-          onPress={this.onChange}
+      <View style={{padding: 8, backgroundColor: 'white'}}>
+        <SegmentedControlTab
+          tabTextStyle={{color: '#333'}}
+          tabStyle={{borderColor: '#333'}}
+          activeTabStyle={{backgroundColor: '#333'}}
+          values={['Location', 'Job Category', 'Job Type']}
+          selectedIndex={this.state.selectedIndex}
+          onTabPress={this.handleIndexChange}
         />
-      </ScrollView>
-    );
-  }
-}
-
-class Filter extends React.Component {
-  static defaultProps = {};
-
-  shouldComponentUpdate(nextProps) {
-    return (
-      this.props.distance !== nextProps.distance ||
-      this.props.jobType.jobType !== nextProps.jobType.jobType ||
-      this.props.jobType.payRange !== nextProps.jobType.payRange ||
-      this.props.categories !== nextProps.categories ||
-      this.props.location.region !== nextProps.location.region ||
-      this.props.location.disctrict !== nextProps.location.disctrict
-    );
-  }
-
-  getDistance = distance => {
-    this.props.dispatch({
-      type: 'queryFilter',
-      payload: {name: 'distance', data: distance},
-    });
-  };
-
-  getJobType = data => {
-    this.props.dispatch({
-      type: 'queryFilter',
-      payload: {name: 'jobType', data: data},
-    });
-  };
-
-  getLocation = data => {
-    this.props.dispatch({
-      type: 'queryFilter',
-      payload: {name: 'location', data: data},
-    });
-  };
-
-  getCategories = data => {
-    this.props.dispatch({
-      type: 'queryFilter',
-      payload: {name: 'categories', data: data},
-    });
-  };
-
-  componentDidMount() {
-    this.props.componentDidMount && this.props.componentDidMount();
-  }
-
-  render() {
-    const FilterArray = ['Location', 'Categories', 'Type'];
-    return (
-      <Toggle>
-        {(ctrl, state, filterItem) => (
-          <React.Fragment>
-            <FilterContainer>
-              {FilterArray.map((item, index) => (
-                <FilterItem
-                  key={index}
-                  style={{width: WIDTH / FilterArray.length}}
-                  onPress={() => ctrl(item)}>
-                  {item}
-                </FilterItem>
-              ))}
-            </FilterContainer>
-            <Modal
-              animationType="none"
-              transparent={true}
-              visible={state}
-              onRequestClose={() => ctrl()}>
-              <TouchableOpacity
-                onPress={ctrl}
-                style={{
-                  height: Platform.OS === 'ios' ? 68 : 48,
-                }}
-              />
-              <Switch active={filterItem} Item={FilterArray}>
-                <Container
-                  h={396}
-                  confirm={data => {
-                    this.getLocation(data);
-                    ctrl();
-                  }}>
-                  {selectFn => (
-                    <LocationSelector
-                      region={this.props.location.region}
-                      disctrict={this.props.location.disctrict}
-                      onChange={data => selectFn(data)}
-                    />
-                  )}
-                </Container>
-                <Container
-                  h={396}
-                  confirm={data => {
-                    this.getCategories(data);
-                    ctrl();
-                  }}>
-                  {selectFn => (
-                    <Categoris
-                      categories={this.props.categories}
-                      onChange={selectFn}
-                    />
-                  )}
-                </Container>
-                <Container
-                  h={396}
-                  confirm={data => {
-                    this.getJobType(data);
-                    ctrl();
-                  }}>
-                  {selectFn => {
-                    return (
-                      <JobType
-                        jobType={this.props.jobType.jobType}
-                        payRange={this.props.jobType.payRange}
-                        onChange={data => selectFn(data)}
-                      />
-                    );
-                  }}
-                </Container>
-              </Switch>
-              <TouchableOpacity
-                onPress={ctrl}
-                activeOpacity={1}
-                style={{
-                  width: WIDTH,
-                  height: HEIGHT,
-                  backgroundColor: 'rgba(0,0,0,0.6)',
-                }}
-              />
-            </Modal>
-          </React.Fragment>
-        )}
-      </Toggle>
+      </View>
     );
   }
 }
